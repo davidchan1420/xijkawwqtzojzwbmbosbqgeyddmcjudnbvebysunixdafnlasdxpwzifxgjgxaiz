b0b287fadd9c07bebb8b3aa568041f2f4a9e7c78
roamerchan <roamerchan@localhost>
2014-07-09 07:46:49 +0800

    .

---
+++ src/main/java/movieservice/domain/SearchCriteria.java
@@ -0,0 +1,109 @@
+package movieservice.domain;
+
+import java.util.Calendar;
+import java.util.List;
+
+import movieservice.util.CalendarUtil;
+
+public class SearchCriteria {
+
+	private String language;
+	private String movieName;
+	private String cinema;
+
+	public class ShowingDate{
+		private Calendar showingDateMin;
+		private Calendar showingDateMax;
+		public Calendar getShowingDateMin() {
+			return showingDateMin;
+		}
+		public Calendar getShowingDateMax() {
+			return showingDateMax;
+		}
+		
+		public void setShowingDate(Calendar showingDate){
+		
+			Calendar showingDateMax = CalendarUtil.trimDayToMax(showingDate.getTime());
+			this.showingDateMax = showingDateMax;
+			
+			Calendar showingDateMin = CalendarUtil.trimDayToMin(showingDate.getTime());
+			this.showingDateMin = showingDateMin;		
+		}
+		
+	}
+	
+	private List<ShowingDate> showingDates;
+
+	private Double x;
+	private Double y;
+	private Integer distanceRange;
+		
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		if(movieName.trim().length() == 0){
+			movieName = null;
+		}
+		this.movieName = movieName;
+	}	
+	
+	public String getCinema() {
+		return cinema;
+	}
+
+	public void setCinema(String cinema) {
+		if(cinema.trim().length() == 0){
+			cinema = null;
+		}
+		this.cinema = cinema;
+	}
+
+	public List<ShowingDate> getShowingDates() {
+		return showingDates;
+	}
+
+	public void setShowingDates(List<ShowingDate> showingDates) {
+		this.showingDates = showingDates;
+	}
+
+	public Double getX() {
+		return x;
+	}
+
+	public void setX(Double x) {
+		this.x = x;
+	}
+
+	public Double getY() {
+		return y;
+	}
+
+	public void setY(Double y) {
+		this.y = y;
+	}
+
+	public Integer getDistanceRange() {
+		return distanceRange;
+	}
+
+	public void setDistanceRange(Integer distanceRange) {
+		if(distanceRange == 0){
+			distanceRange = null;
+		}
+		this.distanceRange = distanceRange;
+	}
+
+	
+
+}
